a37fba100497a7ef5f25d83d436a6591725851e2
Paul <paul.heidenreich@5minds.de>
2018-03-13 15:51:57 +0100

    :tada: Initial commit

---
+++ src/test.ts
@@ -0,0 +1,3 @@
+import {TestInterface1} from '@essential-projects/test_package_1';
+import {TestInterface2} from '@essential-projects/test_package_2';
+import {TestInterface3} from '@essential-projects/test_package_3';
